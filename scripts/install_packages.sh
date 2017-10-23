55a899c41aba8689eda54d4efa295a0dec498605
刘腾辉 <354151733@qq.com>
2017-10-23 20:16:25 +0800

    updated install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,4 +1,6 @@
-pip > /etc/null
+# install_packages.sh
+# check whether it is needed to removing existed low-version pip
+dpkg -l pip > /etc/null
 if [ $? -eq 0 ]
 then echo "removing existed pip ..." && \
 	 sudo apt-get remove pip -y && \
@@ -6,9 +8,11 @@ then echo "removing existed pip ..." && \
 else echo "Updating ..."
 fi  && \
 
+# update 
 sudo apt-get update -y && \
 echo "updated. Changing dir to /home/pi ..." && \
 
+# download setuptools and packages
 cd /home/pi && \
 echo "/home/pi: " && \
 echo -e "\tdownloading setuptools and pip packages ..." && \
@@ -16,10 +20,11 @@ sudo cp -p /home/pi/iptalk_resources/packages/setuptools-36.6.0.zip /home/pi/set
 sudo cp -p /home/pi/iptalk_resources/packages/pip-9.0.1.tar.gz /home/pi/pip-9.0.1.tar.gz && \
 echo -e "\tsetuptools and pip packages downloaded. Changing dir to /home/pi/packages ..." && \
 
+# install setuptools first
 cd /home/pi/packages && \
 echo "/home/pi/packages: " && \
 echo -e "\tUnziping setuptools ..." && \
-unzip setuptools-36.6.0 && \
+unzip -o setuptools-36.6.0 && \
 echo -e "\tsetuptools unzipped. Changing dir to setuptools-36.6.0" && \
 cd setuptools-36.6.0 && \
 echo "/home/pi/packages/setuptools-36.6.0: "
@@ -27,6 +32,7 @@ echo -e "\t\tinstalling setuptools ..." && \
 python setup.py install && \
 echo -e "\t\tsetuptools installed. Changing dir to /home/pi/packages ..." && \
 
+# install pip after setuptools
 cd /home/pi/packages && \
 echo "/home/pi/packages: " && \
 echo -e "\tunpackaging pip ..." && \
@@ -43,17 +49,23 @@ hash -r && \
 sleep 1 && \
 echo -e "\t\tpip path corrected. Changing dir to /home/pi/iptalk_resources ..." && \
 
+# install other dependencies python packages for iptalk application
 cd /home/pi/iptalk_resources && \
 echo "/home/pi/iptalk_resources: " && \
 echo -e "\tinstalling packages ..." && \
-cat /home/pi/peptides.txt | while read line
+cat requirements.txt | while read line
 do
 	echo -e "---------- ---------- ----------" && \
-   	echo -e "\tinstalling " $line " ..." && \
-   	sudo pip install --no-index --find-links=/home/pi/iptalk_resources/packages $line && \
+   	echo -e "\tinstalling" $line " ..." && \
+   	pkg=`tr A-Z a-z <<< $line`
+   	if [ pkg == twisted ]
+   	then sudo apt-get install -y python-Twisted
+   	else sudo pip install --no-index --find-links=/home/pi/iptalk_resources/packages $line 
+   	fi && \
    	echo -e "\t" $line " installed." 
 done && \
 echo -e "\t-------- all pkgs installed --------" && \
 
+# finnally check
 echo "please check: " && \
 pip list
