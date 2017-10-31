63e888ebcb00cf660b15cd909f1ec1808a45f814
刘腾辉 <354151733@qq.com>
2017-10-31 14:51:17 +0800

    corrected install_ffmpeg.sh

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -6,5 +6,6 @@ echo -e "\tdownloading ffmpeg ..." && \
 sudo cp -p /home/pi/iptalk_resources/packages/ffmpeg-release-armel-32bit-static.tar.xz /home/pi/ffmpeg.tar.xz && \
 echo -e "\tffmpeg downloaded. Unpackaging ffmpeg ..." && \
 tar -Jxf ffmpeg.tar.xz && \
+sudo mv /home/pi/ffmpeg-3.3.4-armel-32bit-static /home/pi/ffmpeg
 echo -e "\tffmpeg unpackaged. Static ffmpeg is ready for use." && \
-echo -e "\t-------- install_ffmpeg.sh finished --------" && \
+echo -e "\t-------- install_ffmpeg.sh finished --------"
