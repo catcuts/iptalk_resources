4c8ab0ac337fcdefdede88c689adc6f39d841412
catcuts <354151733@qq.com>
2017-11-29 19:57:48 +0800

    added ufw reset before all setting begings

--- scripts/active_ufw.sh
+++ scripts/active_ufw.sh
@@ -11,6 +11,7 @@ else
      echo -e "\tufw installed. ufw configing ..."
 fi  && \
 
+sudo ufw reset && \
 sudo ufw allow from 10.0.0.0/8 to any port 22 && \
 sudo ufw allow from 172.16.0.0/12 to any port 22 && \
 sudo ufw allow from 192.168.0.0/16 to any port 22 && \
