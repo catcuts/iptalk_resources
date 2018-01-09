3341d0e5c20ff041f08bb82e36cda43fc964d56a
catcuts <354151733@qq.com>
2018-01-09 08:32:03 +0800

    corrected start.sh

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -5,19 +5,19 @@ if [[ -e $MYSQL_HOST ]]; then
 fi
 
 if [[ -e $MYSQL_PORT ]]; then
-    sed "s/port\s*=\s*.*/port = $MYSQL_HOST/"
+    sed "s/port\s*=\s*.*/port = $MYSQL_PORT/"
 fi
 
 if [[ -e $IPTALK_DATABASE ]]; then
-    sed "s/iptalkdatabase\s*=\s*.*/iptalkdatabase = $MYSQL_HOST/"
+    sed "s/iptalkdatabase\s*=\s*.*/iptalkdatabase = $IPTALK_DATABASE/"
 fi
 
 if [[ -e $MYSQL_USERNAME ]]; then
-    sed "s/user\s*=\s*.*/user = $MYSQL_HOST/"
+    sed "s/user\s*=\s*.*/user = $MYSQL_USERNAME/"
 fi
 
 if [[ -e $IPTALK_PASSWORD ]]; then
-    sed "s/password\s*=\s*.*/password = $MYSQL_HOST/"
+    sed "s/password\s*=\s*.*/password = $IPTALK_PASSWORD/"
 fi
 
 mkdir -p /var/run/sshd && \
