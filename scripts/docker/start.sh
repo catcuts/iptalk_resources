4ebc60f2e2f2d402f59b4cd6318c22578b4c8220
catcuts <354151733@qq.com>
2018-03-03 11:34:53 +0800

    updated and tested

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -1,34 +1,31 @@
 #!/usr/bin/bash
 
-awk 'BEGIN { cmd="cp -ri /home/pi/tmp/data/* /home/pi/src/data"; print "n" |cmd; }'
-awk 'BEGIN { cmd="cp -ri /home/pi/tmp/settings/* /home/pi/src/settings"; print "n" |cmd; }'
+#awk 'BEGIN { cmd="cp -ri /home/pi/tmp/data/* /home/pi/src/data"; print "n" |cmd; }'
+#awk 'BEGIN { cmd="cp -ri /home/pi/tmp/settings/* /home/pi/src/settings"; print "n" |cmd; }'
+
+cp -rn /home/pi/tmp/data/* /home/pi/src/data
+cp -rn /home/pi/tmp/settings/* /home/pi/src/settings
 
 setting_file=/home/pi/src/settings/default.ini
 
-echo $MYSQL_HOST
-echo $MYSQL_PORT
-echo $IPTALK_DATABASE
-echo $MYSQL_USERNAME
-echo $IPTALK_PASSWORD
-
-if [[ -e $MYSQL_HOST ]]; then
-    sed "s/host\s*=\s*.*/host = $MYSQL_HOST/" $setting_file
+if [[ $MYSQL_HOST ]]; then
+    sed -in "s/^host\s*=\s*.*/host = $MYSQL_HOST/" $setting_file
 fi
 
-if [[ -e $MYSQL_PORT ]]; then
-    sed "s/port\s*=\s*.*/port = $MYSQL_PORT/" $setting_file
+if [[ $MYSQL_PORT ]]; then
+    sed -in "s/^port\s*=\s*.*/port = $MYSQL_PORT/" $setting_file
 fi
 
-if [[ -e $IPTALK_DATABASE ]]; then
-    sed "s/iptalkdatabase\s*=\s*.*/iptalkdatabase = $IPTALK_DATABASE/" $setting_file
+if [[ $IPTALK_DATABASE ]]; then
+    sed -in "s/^iptalkdatabase\s*=\s*.*/iptalkdatabase = $IPTALK_DATABASE/" $setting_file
 fi
 
-if [[ -e $MYSQL_USERNAME ]]; then
-    sed "s/user\s*=\s*.*/user = $MYSQL_USERNAME/" $setting_file
+if [[ $MYSQL_USERNAME ]]; then
+    sed -in "s/^user\s*=\s*.*/user = $MYSQL_USERNAME/" $setting_file
 fi
 
-if [[ -e $IPTALK_PASSWORD ]]; then
-    sed "s/password\s*=\s*.*/password = $IPTALK_PASSWORD/" $setting_file
+if [[ $MYSQL_PASSWORD ]]; then
+    sed -in "s/^password\s*=\s*.*/password = $MYSQL_PASSWORD/" $setting_file
 fi
 
 mkdir -p /var/run/sshd > /etc/null && \
